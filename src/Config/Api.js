3873cada7c67ea26618bdcf66418881964bb6cd5
it13manoj <it13manoj@gmail.com>
2026-01-08 01:24:56 +0530

    udpate

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -1,7 +1,7 @@
 // api.js
 const API = {
-  BASE_URL: "http://192.168.29.21:4800/api/v1/",
-  BASE_URL_IMAGES: "http:///192.168.29.21:4800/api/v1/uploads/",
+  BASE_URL: "https://api.parakshtach.com/api/v1/",
+  BASE_URL_IMAGES: "https://api.parakshtach.com/api/v1/uploads/",
 };
 
 export default API;
